af696781356a184694988fbb4d6c8349c923a17e
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2024-10-01 12:58:40 -0400

    Update psql_test.go

--- db/psql_test.go
+++ db/psql_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestDBConnection(t *testing.T) {
-	p := NewPSQL("localhost", "5432", "alex", "jiayaojie0715", "newdb")
+	p := NewPSQL("localhost", "5432", "alex", "", "newdb")
 	err := p.Connect()
 	if err != nil {
 		t.Errorf("Error connecting to the database: %v", err)
